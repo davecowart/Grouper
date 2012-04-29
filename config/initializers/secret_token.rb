eb6145e96ed083a292415325c1772e9a1c2932fa
Dave Cowart <davecowart@gmail.com>
2012-04-29 16:48:23 -0500

    empty project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Grouper::Application.config.secret_token = '751478e4d8f66cfd1865ed3ff1b4c5a8599e309fe55f8c11259d69ee3f0941b1fece534eabbde83c0a12e8c49911cd8ebc858b8331fcf993cf670fc8305a4fce'
